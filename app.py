f88171195d4c02bd79c77e4351e0bb79ad39d887
nawalse <nawalse@Naoual-SE.>
2025-01-17 17:49:41 +0100

    first_commit

---
+++ app.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import streamlit as st
+import duckdb
+
+st.write("Spaced Repetition System SQL practice")
+
+option = st.selectbox(
+    "What would you like to review ?",
+    ("Joins ", "Group By", "Windows Functions"),
+    index= None,
+    placeholder="Select a theme..."
+)
+
+st.write("You Selected:", option)
+
+
+data = {"a": [1,2,3], "b": [4,5,6]}
+df  = pd.DataFrame(data)
+tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+
+with (tab1):
+    sql_query = st.text_area(label="entrez votre input")
+    result = duckdb.query(sql_query).df()
+    st.write(f"Vous avez entré la query suivante: {sql_query}")
+    st.dataframe(result)
+
+with tab2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+
+with tab3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
